10579991d30911ed44cc5a2bee0c2d7ed84df005
Matej Kňazík <mato.knazik@gmail.com>
2018-03-09 00:39:31 +0100

    client update

--- Makefile
+++ Makefile
@@ -1,17 +1,18 @@
 CC=g++
-CFLAGS= -std=c++11 -I. 
+CFLAGS=-I.
+CXXFLAGS= -std=c++11
 
 ipk-client: ipk-client.o
-	$(CC) -o ipk-client ipk-client.o $(CFLAGS)
+	$(CC) $(CXXFLAGS) -o ipk-client ipk-client.o $(CFLAGS)
 
 ipk-server: ipk-server.o
-	$(CC) -o ipk-server ipk-server.o $(CFLAGS)
+	$(CC) $(CXXFLAGS) -o ipk-server ipk-server.o $(CFLAGS)
 
 clean:
 	rm -f ipk-client.o ipk-server.o
 
 testc: ipk-client
-	./ipk-client
+	./ipk-client -h 127.0.0.1 -p 6666 
 
 tests: ipk-server
 	./ipk-server
--- ipk-client.cpp
+++ ipk-client.cpp
@@ -1,7 +1,12 @@
 #include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <arpa/inet.h>
+#ifdef _WIN32
+  #include <winsock2.h>
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,13 +18,12 @@
 #include <map>
 using namespace std;
 
-class Options {
-  public:
-    bool hFlag = false;
-    bool pFlag = false;
-    bool nFlag = false;
-    bool fFlag = false;
-    bool lFlag = false;
+struct Options {
+    bool hFlag;
+    bool pFlag;
+    bool nFlag;
+    bool fFlag;
+    bool lFlag;
     string host;
     string port;
     string login;
@@ -33,50 +37,119 @@ bool alreadySet(bool flag,int opt) {
   return false;
 }
 
-int parseOptions(int argc, char *argv[],Options option) {
+int parseOptions(int argc, char *argv[],Options *option) {
+  option->hFlag = false;
+  option->pFlag = false;
+  option->nFlag = false;
+  option->fFlag = false;
+  option->lFlag = false;
+
   int opt;
   while ((opt = getopt (argc, argv, "h:p:nfl")) != -1) {
     switch (opt) {
       case 'h':
-        if (alreadySet(option.hFlag,opt)) return -2;
-        option.host = optarg;
-        cout << "-h: " << option.host << "\n";
+        if (alreadySet(option->hFlag,opt)) return -2;
+        option->host = optarg;
+        option->hFlag = true;
         break;
       case 'p':
-        if (alreadySet(option.pFlag,opt)) return -2;
-        option.port = optarg;
-        cout << "-p: " << option.port << "\n";
+        if (alreadySet(option->pFlag,opt)) return -2;
+        option->port = optarg;
+        option->pFlag = true;
         break;
       case 'n':
-        if (alreadySet(option.nFlag,opt)) return -2;
-        cout << "-n\n";
+        if (alreadySet(option->nFlag,opt)) return -2;
+        if (option->fFlag || option->lFlag) {
+          cerr << "ERROR -2: there can be selected only one option of [-n -f -l]\n";
+          return -2;
+        }
+        option->nFlag = true;
         break;
       case 'f':
-        if (alreadySet(option.fFlag,opt)) return -2;
-        cout << "-f\n";
+        if (alreadySet(option->fFlag,opt)) return -2;
+        if (option->nFlag || option->lFlag) {
+          cerr << "ERROR -2: there can be selected only one option of [-n -f -l]\n";
+          return -2;
+        }
+        option->fFlag = true;
         break;
       case 'l':
-        if (alreadySet(option.lFlag,opt)) return -2;
-        cout << "-l\n";
+        if (alreadySet(option->lFlag,opt)) return -2;
+        if (option->fFlag || option->nFlag) {
+          cerr << "ERROR -2: there can be selected only one option of [-n -f -l]\n";
+          return -2;
+        }
+        option->lFlag = true;
         break;
       default:
-        cerr << "ERROR -1: invalid option: \"-" << (char)optopt << "\" \n";
+        cerr << "ERROR -1: invalid option: \"-" << (char)optopt << "\" usage: ./ipk-client -h host -p port [-n|-f|-l] login \n";
         return -1;
       }
   }
-  if (optind == argc - 1) {
-    option.login = argv[optind];
-    cout << "login:" << option.login << "\n";
+
+  if (!option->hFlag || !option->pFlag) {
+    cerr << "ERROR -1: wrong arguments\n";
+    return -1;
   }
-  return EXIT_SUCCESS;
+
+  if (option->nFlag) {
+    if (optind+1==argc) {
+      option->login = argv[optind];
+      return 1;
+    } else {
+      cerr << "ERROR -2: missing login argument\n";
+      return -2;
+    }
+  }
+  if (option->fFlag) {
+    if (optind+1==argc) {
+      option->login = argv[optind];
+      return 2;
+    } else {
+      cerr << "ERROR -2: missing login argument\n";
+      return -2;
+    }
+  }
+  if (option->lFlag) {
+    if (optind+1==argc) {
+      option->login = argv[optind];
+    }
+    return 3;
+  }
+
+  if (optind+1==argc) {
+    option->login = argv[optind];
+  } else {
+    cerr << "ERROR -2: missing login argument\n";
+    return -2;
+  }
+
+  return 0;
 }
 
 int main(int argc,char *argv[]) {
 
-  Options option;
-  int invalidOpt = parseOptions(argc,argv,option);
-  if (invalidOpt) return invalidOpt;
+  struct Options options;
+  int opt = parseOptions(argc,argv,&options);
+  if (opt < 0) return opt;
 
+  int clientSocket = socket(AF_INET, SOCK_STREAM,0);
+  if (clientSocket <= 0) {
+    cerr << "ERROR -3: socket creation failure\n";
+    return -3;
+  }
+
+  sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET; // adress family = internet
+  int port = stoi(options.port,nullptr);
+  serverAddr.sin_port = htons(port); // port assigmnet
+  serverAddr.sin_addr.s_addr = inet_addr(options.host.c_str()); // set adress to host
   
+
+
+
+
+
+
   return EXIT_SUCCESS;
 }
